6160bd4373c37d2162f984bcf21b1285933c4dd2
pharanyu <pharanyu.c@codium.co>
2020-10-07 14:53:00 +0700

    update 4,5,6,7

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,9 +1,33 @@
+import { checkServerIdentity } from "tls";
+
 function validateStrOfNumber(number: string): string {
     if (/[,|\n]{2,}/.test(number)) {
         throw 'Vlidate error';
     }
 
     return number;
+}
+
+function getData(text: string): [string, string] {
+    const hasDelimiter: boolean = text.indexOf('//') === 0 && text.search('\n') !== -1;
+
+    let delimiter: string = ',';
+    if (hasDelimiter) {
+        delimiter = text.substring(2, text.indexOf('\n'));
+    }
+
+    let numberStr: string = hasDelimiter 
+        ? text.substring(text.indexOf('\n') + 1) 
+        : text;
+    
+    return [delimiter, numberStr]
+}
+
+function checkNegativeNumber(numbers: number[]) {
+    const negatives: number[] = numbers.filter(n => n < 0) || [];
+    if (negatives.length > 0) {
+        throw 'negatives not allowed ' + negatives.join(',')
+    }
 }
 
 /**
@@ -13,11 +37,23 @@ function validateStrOfNumber(number: string): string {
  * @returns      number 
  */
 export function Add(number:string): number {
-    const validatedNumber: string = validateStrOfNumber(number);
+    const [delimiter, numberStr] = getData(number);
 
-    const numbers: number[] = validatedNumber.split(/,|\n/).map(str => +str);
+    const validatedNumber: string = validateStrOfNumber(numberStr);
+
+    const regexSplit = new RegExp(`${delimiter}|\n`);
+    const numbers: number[] = validatedNumber
+        .split(regexSplit)
+        .map(str => +str)
+        .filter(num => num <= 1000);
+
+    checkNegativeNumber(numbers);
 
     const sumNumber: number = numbers.reduce((sum, number) => sum + number, 0);
 
     return sumNumber;
 }
+
+// let a = "//;\n".startsWith('//')
+
+console.log(getData('//;\n1;2'));
--- project/src/tests/main.test.ts
+++ project/src/tests/main.test.ts
@@ -26,41 +26,41 @@ describe('romantoInt Tests', () => {
     expect(testvalue).equal(value);
   });
 
-  it('"-1, 345" should be 344', () => {
-    // given
-    const num = '-1, 345';
-    const value = 344;
+  // it('"-1, 345" should be 344', () => {
+  //   // given
+  //   const num = '-1, 345';
+  //   const value = 344;
 
-    // when
-    const testValue = Add(num);
+  //   // when
+  //   const testValue = Add(num);
 
-    // then
-    expect(testValue).equal(value);
-  })
+  //   // then
+  //   expect(testValue).equal(value);
+  // })
 
-  it('"-1,-2" should be -3', () => {
-    // given
-    const num = '-1,-2';
-    const value = -3;
+  // it('"-1,-2" should be -3', () => {
+  //   // given
+  //   const num = '-1,-2';
+  //   const value = -3;
 
-    // when
-    const testValue = Add(num);
+  //   // when
+  //   const testValue = Add(num);
 
-    // then
-    expect(testValue).equal(value);
-  })
+  //   // then
+  //   expect(testValue).equal(value);
+  // })
 
-  it('"10,11, -1, 0" should be 20', () => {
-    // given
-    const num = '10,11, -1, 0';
-    const value = 20;
+  // it('"10,11, -1, 0" should be 20', () => {
+  //   // given
+  //   const num = '10,11, -1, 0';
+  //   const value = 20;
 
-    // when
-    const testValue = Add(num);
+  //   // when
+  //   const testValue = Add(num);
 
-    // then
-    expect(testValue).equal(value);
-  })
+  //   // then
+  //   expect(testValue).equal(value);
+  // })
 
   it('"1\n2,3" should be 6', () => {
     // given
@@ -93,6 +93,67 @@ describe('romantoInt Tests', () => {
     // then
     expect(() => {Add(num)}).to.throw();
   })
+
+  it('//;\n1;2 should be 3', () => {
+    // given
+    const num = '//;\n1;2';
+    const value = 3;
+
+    // when
+    const testValue = Add(num);
+
+    // then
+    expect(testValue).equal(value);
+  }) 
+
+  it('//_\n1_2 should be 1', () => {
+    // given
+    const num = '//_\n1_2';
+    const value = 3;
+
+    // when
+    const testValue = Add(num);
+
+    // then
+    expect(testValue).equal(value);
+  }) 
+
+  it('1,2,-3,-4,5,-6 should be throw "negatives not allowed -3,-4,-6"', () => {
+    // given
+    const num = '1,2,-3,-4,5,-6';
+    const value = 3;
+
+    // when
+
+    // then
+    expect(() => { Add(num) }).to.throw('negatives not allowed -3,-4,-6');
+  })
+  
+  it('2,1001 should be 2', () => {
+    // given
+    const num = '2,1001';
+    const value = 2;
+
+    // when
+    const testValue = Add(num);
+
+    // then
+    expect(testValue).equal(value);
+  })
+  
+  it('//[***]\n1***2***3 should be 6', () => {
+    // given
+    const num = '//[***]\n1***2***3';
+    const value = 6;
+
+    // when
+    const testValue = Add(num);
+
+    // then
+    expect(testValue).equal(value);
+  }) 
+
+
 });
 
 describe('xxx Tests', () => {
